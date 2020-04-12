1d18fcd495e87a6f3c21b51f6f364287e9c702c8
Anas shamia <a.shamia@dce.sa>
2020-04-12 12:07:39 +0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,20 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-const cssnano = require('cssnano');
+// const purgecss = require('@fullhuman/postcss-purgecss');
+// const cssnano = require('cssnano');
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        cssnano({
-            preset: 'default'
-        }),
-        purgecss({
-            content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx',
-                './node_modules/v-calendar/src/**/*.vue',
-                './node_modules/vue-datetime/src/*.vue',
-                './node_modules/vue-select/src/**/*.vue',
-            ],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
+        // cssnano({
+        //     preset: 'default'
+        // }),
+        // purgecss({
+        //     content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx',
+        //         './node_modules/v-calendar/src/**/*.vue',
+        //         './node_modules/vue-datetime/src/*.vue',
+        //         './node_modules/vue-select/src/**/*.vue',
+        //         './node_modules/vue-data-tables/src/**/*.vue',
+        //     ],
+        //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        // })
     ]
 };
